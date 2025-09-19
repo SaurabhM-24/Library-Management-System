71e2ccc0de09a29989627eaca26f77f2c1bfda7a
Saurabh Mishra <saurabhmishra.24x@gmail.com>
2025-09-19 23:56:08 +0530

    Update Library Management System using CSV.py

--- Library Management System using CSV.py
+++ Library Management System using CSV.py
@@ -7,6 +7,8 @@ def NewBooks():
         W=csv.writer(F)
         W.writerow(['BookID','Name','Author','Genre1','Genre2','Genre3','Copies','Issued','SC','Price','Ext URL1','Ext URL2'])
         ID=1001
+
+        
         while True:            
             Name=input('\nEnter the name of book : ')
             Author=input('Enter the name of the author : ')
@@ -1058,6 +1060,7 @@ while True:
         break
     else:
         print('\nPlease enter a valid option')
+
 
 
 
